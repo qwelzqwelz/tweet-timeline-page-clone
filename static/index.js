04c75e7c8e8a4f5b73c78040e28c87264f263cd7
qwelzqwelz <qwelzqwelz@126.com>
2022-08-16 21:43:03 +0800

    :sparkles: feat: (image-viewer) 添加移动端适配 & 图片浏览器

---
+++ static/index.js
@@ -0,0 +1,29 @@
+const PHOTO_VIEWER_SETTINGS = {
+    toolbar: {
+        zoomIn: 4,
+        zoomOut: 4,
+        oneToOne: 4,
+        reset: 4,
+        prev: {
+            show: 1,
+            size: "large",
+        },
+        play: 0,
+        next: {
+            show: 1,
+            size: "large",
+        },
+        rotateLeft: 4,
+        rotateRight: 4,
+        flipHorizontal: 4,
+        flipVertical: 4,
+    },
+};
+
+// 视频播放器
+$(".video-box").twitterVideoPlayer();
+
+// 图片浏览
+$(".media-photos-container").each(function() {
+    new Viewer(this, PHOTO_VIEWER_SETTINGS);
+});
